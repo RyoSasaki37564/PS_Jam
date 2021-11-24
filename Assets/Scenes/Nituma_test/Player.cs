4831721ab64e5381c091395de4a76fda123bfa9e
sakezuma <sakezuma@gmail.com>
2021-11-24 16:15:09 +0900

    Playerのパワーを表示

--- Assets/Scenes/Nituma_test/Player.cs
+++ Assets/Scenes/Nituma_test/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour
     [SerializeField] public static int _money = 0;
     [SerializeField] Text _playerpower = default;
 
+    private void Update()
+    {
+        _playerpower.text = "Power: " + _power.ToString();
+    }
     public void AddPower(int x)
     {
         _power += x;   
