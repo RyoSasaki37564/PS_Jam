c8f7542f577374611ba6335d7a684ecc33c7e6fa
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-11-24 17:58:40 +0900

    コモン作った

---
+++ Assets/Comon.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Comon : MonoBehaviour
+{
+    [SerializeField] int _power = 5;
+    private void Start()
+    {
+        FindObjectOfType<Player>().AddPower(_power);
+    }
+}
