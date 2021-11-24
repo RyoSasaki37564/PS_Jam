8a21741acfa79df2076718f7a85439f0899a3d44
sakezuma <sakezuma@gmail.com>
2021-11-24 16:00:53 +0900

    Player

--- Assets/Scenes/Nituma_test/Player.cs
+++ Assets/Scenes/Nituma_test/Player.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public static int _power = 0;
     [SerializeField] public static int _money = 0;
+    [SerializeField] Text _playerpower = default;
 
     public void AddPower(int x)
     {
