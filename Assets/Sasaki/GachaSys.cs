76b84651c6b36cf01eecb92d22917db3edd7f976
DESKTOP-4NV9MP5\vantan <sskryo0607@gmail.com>
2021-11-24 16:42:05 +0900

    ガチャ整え

--- Assets/Sasaki/GachaSys.cs
+++ Assets/Sasaki/GachaSys.cs
@@ -11,6 +11,8 @@ public class GachaSys : MonoBehaviour
     [SerializeField] List<GameObject> m_CanHitCharactors_SSR = new List<GameObject>(); //排出SSRキャラリスト
     [SerializeField] List<GameObject> m_CanHitCharactors_SR = new List<GameObject>(); //排出SRキャラリスト
     [SerializeField] List<GameObject> m_CanHitCharactors_R = new List<GameObject>(); //排出Rキャラリスト
+    [SerializeField] List<GameObject> m_CanHitCharactors_C = new List<GameObject>(); //排出ザコキャラリスト
+    [SerializeField] List<GameObject> m_CanHitCharactors_M = new List<GameObject>(); //排出地雷キャラリスト
 
 
     [SerializeField] public int MoneyInPockets;
@@ -23,20 +25,32 @@ public class GachaSys : MonoBehaviour
 
     [SerializeField] GameObject m_ensyutu = default;
 
-    [SerializeField]int m_kakaku = 5;
+    [SerializeField] int m_kakaku0 = 5;
+    [SerializeField] int m_kakaku1 = 10;
+    [SerializeField] int m_kakaku2 = 1;
+
+    public enum Turn
+    {
+        InputTurn,
+        BattleTurn,
+        GameSetTurn,
+    }
+    public static Turn m_turn = Turn.InputTurn;
 
     void Start()
     {
+        m_turn = Turn.InputTurn;
+
         m_ensyutu.SetActive(false);
         _moneyText.text = "MONEY: " + MoneyInPockets.ToString();
         m_ResultDsp.SetActive(false);
     }
 
-    public virtual void Gacha()
+    public void Gacha0()
     {
         m_ensyutu.SetActive(true);
-        UseMoney(m_kakaku);
-        if (MoneyInPockets >= m_kakaku)
+        UseMoney(m_kakaku0);
+        if (MoneyInPockets >= m_kakaku0)
         {
             m_ResultDsp.SetActive(true);
 
@@ -76,11 +90,119 @@ public class GachaSys : MonoBehaviour
         }
         m_jibun.SetActive(false);
     }
+    public void Gacha1()
+    {
+        m_ensyutu.SetActive(true);
+        UseMoney(m_kakaku1);
+        if (MoneyInPockets >= m_kakaku1)
+        {
+            m_ResultDsp.SetActive(true);
+
+            int rondom = Random.Range(0, 100);
+
+            if (rondom < 50)
+            {
+                //当選キャラをリザルトに表示する。
+                int ssr = Random.Range(0, m_CanHitCharactors_SSR.Count);
+                var x = Instantiate(m_CanHitCharactors_SSR[ssr]);
+                x.SetActive(true);
+                x.transform.SetParent(m_gachaResultPanel.transform);
+                x.transform.localScale = m_CanHitCharactors_SSR[ssr].transform.localScale;
+                x.transform.localPosition = new Vector3(0, 0, 0);
+
+            }
+            else if (rondom < 91)
+            {
+                int c = Random.Range(0, m_CanHitCharactors_C.Count);
+                var x = Instantiate(m_CanHitCharactors_C[c]);
+                x.SetActive(true);
+                x.transform.SetParent(m_gachaResultPanel.transform);
+                x.transform.localScale = m_CanHitCharactors_C[c].transform.localScale;
+                x.transform.localPosition = new Vector3(0, 0, 0);
+
+            }
+            else
+            {
+                int m = Random.Range(0, m_CanHitCharactors_M.Count);
+                var x = Instantiate(m_CanHitCharactors_M[m]);
+                x.SetActive(true);
+                x.transform.SetParent(m_gachaResultPanel.transform);
+                x.transform.localScale = m_CanHitCharactors_M[m].transform.localScale;
+                x.transform.localPosition = new Vector3(0, 0, 0);
+
+            }
+        }
+        m_jibun.SetActive(false);
+    }
+    public void Gacha2()
+    {
+        m_ensyutu.SetActive(true);
+        UseMoney(m_kakaku2);
+        if (MoneyInPockets >= m_kakaku2)
+        {
+            m_ResultDsp.SetActive(true);
+
+            int rondom = Random.Range(0, 100);
+
+            if (rondom < 1)
+            {
+                //当選キャラをリザルトに表示する。
+                int ssr = Random.Range(0, m_CanHitCharactors_SSR.Count);
+                var x = Instantiate(m_CanHitCharactors_SSR[ssr]);
+                x.SetActive(true);
+                x.transform.SetParent(m_gachaResultPanel.transform);
+                x.transform.localScale = m_CanHitCharactors_SSR[ssr].transform.localScale;
+                x.transform.localPosition = new Vector3(0, 0, 0);
+
+            }
+            else if (rondom < 31)
+            {
+                int r = Random.Range(0, m_CanHitCharactors_R.Count);
+                var x = Instantiate(m_CanHitCharactors_R[r]);
+                x.SetActive(true);
+                x.transform.SetParent(m_gachaResultPanel.transform);
+                x.transform.localScale = m_CanHitCharactors_R[r].transform.localScale;
+                x.transform.localPosition = new Vector3(0, 0, 0);
+
+            }
+            else
+            {
+                int c = Random.Range(0, m_CanHitCharactors_R.Count);
+                var x = Instantiate(m_CanHitCharactors_C[c]);
+                x.SetActive(true);
+                x.transform.SetParent(m_gachaResultPanel.transform);
+                x.transform.localScale = m_CanHitCharactors_C[c].transform.localScale;
+                x.transform.localPosition = new Vector3(0, 0, 0);
+
+            }
+        }
+        m_jibun.SetActive(false);
+    }
     void Update()
     {
-        if (MoneyInPockets <= 0)
+        if (m_turn == Turn.InputTurn && MoneyInPockets <= 0)
         {
-            GameOver = true;
+            //GameOver = true;
+            m_turn = Turn.BattleTurn;
+        }
+
+        if(m_turn == Turn.GameSetTurn)
+        {
+            /*
+             if(tekiPower < jibunPower)
+             {
+                win.
+             }
+             else if(tekiPower == jibunPower)
+             {
+                hikiwake.
+             }
+             else
+             {
+                make.
+                GameOver = true;
+             }
+             */
         }
     }
 
